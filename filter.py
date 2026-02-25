7d490bcfee6cdee7be816a13084f4aefb1d49d50
asrcpq <asrcpq@gmail.com>
2026-02-25 14:18:17 +0900

    upd

--- filter.py
+++ filter.py
@@ -340,11 +340,11 @@ if __name__ == "__main__":
     config_path = Path(sys.argv[1]).resolve()
     with config_path.open("rb") as f:
         config = tomllib.load(f)
-    BASE_DIR = Path(__file__).parent.parent / "reearth-flow/engine/plateau-tiles-test"
-    test_name = config_path.parent.relative_to(BASE_DIR / "testcases").as_posix()
+    TESTING_DATA_DIR = Path(__file__).parent.parent / "reearth-flow/engine/testing/data"
+    test_name = config_path.parent.relative_to(TESTING_DATA_DIR / "testcases").as_posix()
     citymodel_path = Path(__file__).parent / "data"
-    artifacts_base = BASE_DIR / "artifacts"
-    testcase_base = BASE_DIR / "testcases"
+    artifacts_base = TESTING_DATA_DIR / "fixtures/plateau-citymodel"
+    testcase_base = TESTING_DATA_DIR / "testcases"
     zip_name = config["citygml_zip_name"].rsplit("_op_", 1)[0] + "_op.zip"
     src_zip = citymodel_path / zip_name
     extract_zip_to_structure(src_zip, artifacts_base, testcase_base, test_name, config["filter"]["tree"])
